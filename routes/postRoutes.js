3842121f7dbd7c0f67e4510ccc80352dbac91264
Hungnhatit <nhatpro204@gmail.com>
2024-09-19 11:50:16 +0700

    Add some post routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import path from "path";
 import userAuth from "../middleware/authMiddleware.js";
-import { createPost, getComments, getPost, getPosts, getUserPosts } from "../controllers/postController.js";
+import { commentPost, createPost, getComments, getPost, getPosts, getUserPosts, likePost, likePostComment, replyPostComment } from "../controllers/postController.js";
 
 const router = express.Router();
 
@@ -16,5 +16,18 @@ router.post('/get-user-post/:id', userAuth, getUserPosts);
 
 // Comment
 router.get('/comments/:postId', getComments);
+
+
+// Like post and like comment on posts
+router.post('/like/:id', userAuth, likePost);
+router.post('/like-comment/:id/:rid', userAuth, likePostComment); //id: id của comment, rid: id của reply comment
+router.post('/comment/:id', userAuth, commentPost);
+router.post('/reply-comment/:id', userAuth, replyPostComment);
+
+
+// Comment post
+
+
+// Delete post
 
 export default router;
